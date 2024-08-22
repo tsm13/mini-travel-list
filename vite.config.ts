6736ac1e820c5d402b3ff796b48543a820448365
tsm13 <thais@live.jp>
2024-08-22 20:13:51 -0300

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,26 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest: false })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mini Travel List",
+        short_name: "Mini Travel List",
+        description: "Minimalist travel list PWA",
+        icons: [
+          {
+            src: "/images/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "https://mini-travel-list-tsm13.netlify.app",
+        display: "standalone",
+        theme_color: "#27272a",
+        background_color: "#27272a",
+      },
+    }),
+  ],
 });
